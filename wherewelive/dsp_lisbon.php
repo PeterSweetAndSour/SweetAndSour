dd250338a0f95a4c2aa0a7ea7d3b21db01d555f3
petersweetandsour <peter@sweetandsour.org>
2020-05-30 13:00:26 +0100

    New page for Lisbon.

--- wherewelive/dsp_lisbon.php
+++ wherewelive/dsp_lisbon.php
@@ -1,9 +1,20 @@
 <div class="story">
 	<p>We visited Portugal in 2018 and liked it so much that we decided to move here! My parents happened to be in the UK when I was born allowing me to get a British passport and thus move anywhere within the EU, so long as we did it before Brexit. That was scheduled for 31<sup>st</sup> October 2019 so we arrived just a week before that.</sup></p>
-	<p>I will include some photos here later but we already have a bunch of photos on Google's platform for you to enjoy. Make sure that you turn on the captions or a lot of them won't make sense.</p>
-	<ul>
-		<li><a href="https://photos.app.goo.gl/enKr25uNuk5FoKEt8" target="_blank" class="external">Oct 19 to Dec 19</a> (100 photos)</li>
-		<li><a href="https://photos.app.goo.gl/F3aLYKPT9P728CJv9" target="_blank" class="external">Dec 2019 to Feb 2020</a> (200 photos, or you can see the <a href="https://photos.app.goo.gl/5wEsopKmgfKwFthcA" target="_blank" class="external">summary</a> with 30 photos)</li>
-	</ul>
-	<p>I am also posting occasionally on Instagram <strong>@petersweetandsour</strong>.</p>
+	<p>We have a bunch of photos on Google's platform for you to enjoy. Make sure that you turn on the captions or a lot of them won't make sense.</p>
+</div>
+<div class="clear">
+	<?
+	setThumbnail("album_01.jpg", "https://photos.app.goo.gl/enKr25uNuk5FoKEt8", "youtubeThumbnail");
+	setThumbnail("album_02.jpg", "https://photos.app.goo.gl/F3aLYKPT9P728CJv9", "youtubeThumbnail");
+	setThumbnail("portuguesePaving.jpg", "https://photos.app.goo.gl/5Gxid4aW1eAGn6Fq8", "youtubeThumbnail");
+	?>
+</div>
+
+<div class="story">
+	<p>I also post occasionally on Instagram.</p>
+</div>
+<div class="clear">
+	<?
+	setThumbnail("instagram.jpg", "https://www.instagram.com/petersweetandsour/", "youtubeThumbnail");
+	?>
 </div>
