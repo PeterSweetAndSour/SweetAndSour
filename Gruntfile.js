0f2bf3ab1f454ce5453b04414eb3193f767a3596
petersweetandsour <peter@sweetandsour.org>
2022-05-04 08:53:24 -0400

    Update packages in packages.json and change uglifier since the old one is deprecated. Other files were generated.

--- Gruntfile.js
+++ Gruntfile.js
@@ -96,7 +96,7 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-postcss');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-uglify-es');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	// Default task
