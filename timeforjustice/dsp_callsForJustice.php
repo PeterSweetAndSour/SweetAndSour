2c3337142a76e352258f3102aa2164902ec2e959
petersweetandsour <peter@sweetandsour.org>
2024-05-05 17:58:41 -0400

    Updates for Gaza;

--- timeforjustice/dsp_callsForJustice.php
+++ timeforjustice/dsp_callsForJustice.php
@@ -26,53 +26,59 @@
    that there are two sides, only one of which is widely reported in the US and that the there is widespread
    oppression of one group by the other and that match is utterly uneven.</p> 
    <ul>
-		<li>18May21 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20210518_JoeBiden.txt">Message to President Joe Biden</a> (through whitehouse.gov)</li>
-		<li>5Aug14 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20140805_NBC.pdf">Message to NBC News</a> (145kb)</li>
-		<li>15Jul14 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20140715_IsraeliAmbassador.pdf">Letter to Israeli Ambassador</a> in response to the senseless destruction of the house of Dr Nasser Tatar, director general of Gaza's largest medical facility as noted in <a href="http://www.theguardian.com/world/2014/jul/14/gaza-home-destroyed-israel-shati" class="external" target="_blank">this article</a>. At this point 200 Palestinians had been killed but there were another 1600 after this. (49kb)</li>
-		<li>12Dec11 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20111212_Gingrich.pdf">Message to Newt Gingrich</a> in response to <a href="http://www.guardian.co.uk/world/2011/dec/11/newt-gingrich-palestinian-comments-criticised?intcmp=239" class="external" target="_blank">outrageous comments</a> he made over the weekend. </li>
-		<li>6Jul10 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20100706_CBSNews.pdf">Message to CBS News</a> (38kb)</li>
-		<li>7Jun10 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20100607_MoranWebbWarnerObama.pdf" target="_blank">Letter to Congressman Moran, Senator Webb, Senator Warner and President Obama</a> in response to Israel's deadly raid on aid convoy. When I've sent letters to politians in the past I've often received replies but nothing arrived this time. (77kb)</li>
-		<li>15Mar10 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20100315_JoeBiden.txt" target="_blank">Message to Vice President Joe Biden</a> (though whitehouse.gov) (39kb).</li>
-		<li>29Jan10 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20100130_IsraeliAmbassador.pdf" target="_blank">Letter to Israeli Ambassador</a> (39kb).</li>
+	 	<li>30-Mar-24 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/2024-03-30_Biden.txt">Message to President Joe Biden</a> (through whitehouse.gov)</li>
+	 	<li>19-Feb-24 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/2024-02-19_Biden.txt">Message to President Joe Biden</a> (through whitehouse.gov)</li>
+	 	<li>24-Dec-23 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/2023-12-24_Biden.txt">Message to President Joe Biden</a> (through whitehouse.gov)</li>
+	 	<li>9-Dec-23 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/2023-12-09_Biden.txt">Message to President Joe Biden</a> (through whitehouse.gov)</li>
+	 	<li>12-Nov-23 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/2023-11-12_Biden.txt">Message to President Joe Biden</a> (through whitehouse.gov)</li>
+
+		<li>18-May-21 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20210518_JoeBiden.txt">Message to President Joe Biden</a> (through whitehouse.gov)</li>
+		<li>5-Aug-14 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20140805_NBC.pdf">Message to NBC News</a> (145kb)</li>
+		<li>15-Jul-14 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20140715_IsraeliAmbassador.pdf">Letter to Israeli Ambassador</a> in response to the senseless destruction of the house of Dr Nasser Tatar, director general of Gaza's largest medical facility as noted in <a href="http://www.theguardian.com/world/2014/jul/14/gaza-home-destroyed-israel-shati" class="external" target="_blank">this article</a>. At this point 200 Palestinians had been killed but there were another 1600 after this. (49kb)</li>
+		<li>12-Dec-11 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20111212_Gingrich.pdf">Message to Newt Gingrich</a> in response to <a href="http://www.guardian.co.uk/world/2011/dec/11/newt-gingrich-palestinian-comments-criticised?intcmp=239" class="external" target="_blank">outrageous comments</a> he made over the weekend. </li>
+		<li>6-Jul-10 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20100706_CBSNews.pdf">Message to CBS News</a> (38kb)</li>
+		<li>7-Jun-10 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20100607_MoranWebbWarnerObama.pdf" target="_blank">Letter to Congressman Moran, Senator Webb, Senator Warner and President Obama</a> in response to Israel's deadly raid on aid convoy. When I've sent letters to politians in the past I've often received replies but nothing arrived this time. (77kb)</li>
+		<li>15-Mar-10 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20100315_JoeBiden.txt" target="_blank">Message to Vice President Joe Biden</a> (though whitehouse.gov) (39kb).</li>
+		<li>29-Jan-10 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20100130_IsraeliAmbassador.pdf" target="_blank">Letter to Israeli Ambassador</a> (39kb).</li>
 		<li>24Apr09 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20090424_IsraeliAmbassador.pdf" target="_blank">Letter to Israeli Ambassador</a> (25kb).</li>
-		<li>29Jan09 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20090129_60MinutesComments.pdf" target="_blank">Comments on <i>60 Minutes</i> discussion board</a> (76kb).</li>
-		<li>27Jan09 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20090127_Obama.pdf" target="_blank">Letter to President Obama</a> (695kb).</li>
-		<li>27Jan09 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20090127_Clinton.pdf" target="_blank">Letter to Secretary of State Clinton</a> (695kb).</li>
-		<li>25Jan09 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20090125_60Minutes.pdf" target="_blank">Comments and Feedback to <i>60 Minutes</i></a> (77kb).</li>
-		<li>10Jan09 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20090110_ABCNews.pdf" target="_blank">Email to ABC News</a> (27kb).</li>
-		<li>9Jan09 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20090109_Bush.pdf" target="_blank">Letter to President Bush</a> (889kb).</li>
-		<li>6Jan09 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20090106_Cohen.pdf" target="_blank">Email to Washington Post columnist Richard Cohen</a> (32kb).</li>
-		<li>3Jan09 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20090103_WashingtonPostComment.pdf" target="_blank">Comment left on Washington Post website in response to an article</a> (34kb).</li>
-		<li>1Jan09 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20090101_IsraeliAmbassador.pdf" target="_blank">Letter to Israeli Ambassador</a> (41kb).</li>
-		<li>1Jan09 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20090101_EgyptianAmbassador.pdf" target="_blank">Letter to Egyptian Ambassador</a> (19kb).</li>
-		<li>31Dec08 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20081231_ABCNews.pdf" target="_blank">Email to ABC News</a> (26kb).</li>
-		<li>31Dec08 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20081231_Express.pdf" target="_blank">Email to Express newspaper</a> (498kb).</li>
+		<li>29-Jan-09 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20090129_60MinutesComments.pdf" target="_blank">Comments on <i>60 Minutes</i> discussion board</a> (76kb).</li>
+		<li>27-Jan-09 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20090127_Obama.pdf" target="_blank">Letter to President Obama</a> (695kb).</li>
+		<li>27-Jan-09 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20090127_Clinton.pdf" target="_blank">Letter to Secretary of State Clinton</a> (695kb).</li>
+		<li>25-Jan-09 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20090125_60Minutes.pdf" target="_blank">Comments and Feedback to <i>60 Minutes</i></a> (77kb).</li>
+		<li>10-Jan-09 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20090110_ABCNews.pdf" target="_blank">Email to ABC News</a> (27kb).</li>
+		<li>9-Jan-09 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20090109_Bush.pdf" target="_blank">Letter to President Bush</a> (889kb).</li>
+		<li>6-Jan-09 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20090106_Cohen.pdf" target="_blank">Email to Washington Post columnist Richard Cohen</a> (32kb).</li>
+		<li>3-Jan-09 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20090103_WashingtonPostComment.pdf" target="_blank">Comment left on Washington Post website in response to an article</a> (34kb).</li>
+		<li>1-Jan-09 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20090101_IsraeliAmbassador.pdf" target="_blank">Letter to Israeli Ambassador</a> (41kb).</li>
+		<li>1-Jan-09 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20090101_EgyptianAmbassador.pdf" target="_blank">Letter to Egyptian Ambassador</a> (19kb).</li>
+		<li>31-Dec-08 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20081231_ABCNews.pdf" target="_blank">Email to ABC News</a> (26kb).</li>
+		<li>31-Dec-08 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20081231_Express.pdf" target="_blank">Email to Express newspaper</a> (498kb).</li>
 	 
-		<li>5Oct08 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20081005_WashPostEditorial.pdf" target="_blank">Washington Post Editorial</a> (obviously I did not write this).</li>
-		<li>25Feb07 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20070225_NPR.pdf" target="_blank">Email to NPR</a> after they spoke with the Israeli Ambassador.</li>
-		<li>26Jul04 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20040726_AustGovt_reply.pdf" target="_blank">Reply from Australian government</a> although again, I can't find the original.</li>
-		<li>24Jun04 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20040624_Bush_reply.pdf" target="_blank">Reply from President Bush</a> (500kb) but I've lost the letter I sent!</li>
-		<li>7Oct03 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20031007_Bush.pdf" target="_blank">Letter to President Bush</a> (110kb).</li>
-		<li>29Sep02 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20020929_60Minutes.pdf" target="_blank">Letter to <i>60 Minutes</i></a> (35kb).</li>
-		<li>Sep02 (?) <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20020900_Newsweek.pdf" target="_blank">Letter to Newsweek</a> (1.2MB). This was written in response to a letter suggesting I renew my subscription. Unfortunately I didn't date it but it is after the bombing mentioned above and "two years" after the start of the second <i>intifada</i> which places it at September 2002.</li>
-		<li>23Jul02 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20020723_DenverPost.pdf" target="_blank">Newspaper clipping</a> (1.1MB) after Israeli used a US-made plane to drop a US-made bomb on an apartment building with the aim of assassinating a Hamas leader. I wrote a <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20020818_NighthorseAllardDeGette.pdf">letter to Senators Nighthorse-Campbell &amp; Allard and Congresswoman DeGette</a> (218kb) and include here the <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20020818_Nighthorse_reply.pdf">reply from Nighthorse-Campbell</a> (402kb) and the <a href="files/20020818_Allard_reply.pdf">reply from Allard</a> (767kb).</li>
-		<li>6May02 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20020506_Armey.pdf" target="_blank">Letter to Congressman Dick Armey</a> (23kb) and his <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20020506_Armey_reply.pdf">reply</a> (530kb)</li>
-		<li>22Apr02 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20020422_Bush.pdf" target="_blank">Letter to President Bush</a> (26kb).</li>
-		<li>16Apr02 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20020416_Wolfowitz.pdf" target="_blank">Letter to Paul Wolfowitz</a> (16kb).</li>
-		<li>15Apr02 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20020415_Rooney.pdf" target="_blank">Letter to Andy Rooney of <i>60 Minutes</i> (20kb).</a></li>
-		<li>9Apr02 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20020409_IsraeliEmbassy.pdf" target="_blank">Letter to Israeli Ambassador</a> (21kb).</li>
-		<li>1Apr02 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20020401_60Minutes.pdf" target="_blank">Letter to <i>60 Minutes</i></a> (62kb).</li>
-		<li>1Feb02 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20020201_60Minutes.pdf" target="_blank">Letter to <i>60 Minutes</i></a> (22kb).</li>
-		<li>20Jul01 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20010720_Newsweek.pdf" target="_blank">Analysis of <i>Newsweek</i>'s coverage</a> (7.1MB). I was aware that <i>Newsweek</i>'s coverage was biased but I didn't realize how blatant it was until I did a systematic review. I was surprised that it didn't prompt even a generic response.</li>
+		<li>5-Oct-08 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20081005_WashPostEditorial.pdf" target="_blank">Washington Post Editorial</a> (obviously I did not write this).</li>
+		<li>25-Feb-07 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20070225_NPR.pdf" target="_blank">Email to NPR</a> after they spoke with the Israeli Ambassador.</li>
+		<li>26-Jul-04 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20040726_AustGovt_reply.pdf" target="_blank">Reply from Australian government</a> although again, I can't find the original.</li>
+		<li>24-Jun-04 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20040624_Bush_reply.pdf" target="_blank">Reply from President Bush</a> (500kb) but I've lost the letter I sent!</li>
+		<li>7-Oc-03 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20031007_Bush.pdf" target="_blank">Letter to President Bush</a> (110kb).</li>
+		<li>29-Sep-02 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20020929_60Minutes.pdf" target="_blank">Letter to <i>60 Minutes</i></a> (35kb).</li>
+		<li>Sep-02 (?) <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20020900_Newsweek.pdf" target="_blank">Letter to Newsweek</a> (1.2MB). This was written in response to a letter suggesting I renew my subscription. Unfortunately I didn't date it but it is after the bombing mentioned above and "two years" after the start of the second <i>intifada</i> which places it at September 2002.</li>
+		<li>23-Jul-02 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20020723_DenverPost.pdf" target="_blank">Newspaper clipping</a> (1.1MB) after Israeli used a US-made plane to drop a US-made bomb on an apartment building with the aim of assassinating a Hamas leader. I wrote a <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20020818_NighthorseAllardDeGette.pdf">letter to Senators Nighthorse-Campbell &amp; Allard and Congresswoman DeGette</a> (218kb) and include here the <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20020818_Nighthorse_reply.pdf">reply from Nighthorse-Campbell</a> (402kb) and the <a href="files/20020818_Allard_reply.pdf">reply from Allard</a> (767kb).</li>
+		<li>6-May-02 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20020506_Armey.pdf" target="_blank">Letter to Congressman Dick Armey</a> (23kb) and his <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20020506_Armey_reply.pdf">reply</a> (530kb)</li>
+		<li>22-Apr-02 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20020422_Bush.pdf" target="_blank">Letter to President Bush</a> (26kb).</li>
+		<li>16-Apr-02 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20020416_Wolfowitz.pdf" target="_blank">Letter to Paul Wolfowitz</a> (16kb).</li>
+		<li>15-Apr-02 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20020415_Rooney.pdf" target="_blank">Letter to Andy Rooney of <i>60 Minutes</i> (20kb).</a></li>
+		<li>9-Apr-02 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20020409_IsraeliEmbassy.pdf" target="_blank">Letter to Israeli Ambassador</a> (21kb).</li>
+		<li>1-Apr-02 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20020401_60Minutes.pdf" target="_blank">Letter to <i>60 Minutes</i></a> (62kb).</li>
+		<li>1-Feb-02 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20020201_60Minutes.pdf" target="_blank">Letter to <i>60 Minutes</i></a> (22kb).</li>
+		<li>20-Jul-01 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20010720_Newsweek.pdf" target="_blank">Analysis of <i>Newsweek</i>'s coverage</a> (7.1MB). I was aware that <i>Newsweek</i>'s coverage was biased but I didn't realize how blatant it was until I did a systematic review. I was surprised that it didn't prompt even a generic response.</li>
 	 </ul>
 </div>
 
 <h2>In support of Lebanon</h2>
 <div class="story">
-   <p>I was outraged by the Israelis thought it reasonable to kill over 1000 people, cause billions of dollars of damage and vandalize the environment in response to the kidnapping of two of their soldiers (the Israelis kidnap Palestinians all the time and hold and sometimes torture them) and home-made rocket attacks that I think had killed eight.</p>
+   <p>I was outraged that the Israelis thought it reasonable to kill over 1000 people, cause billions of dollars of damage and vandalize the environment in response to the kidnapping of two of their soldiers (the Israelis kidnap Palestinians all the time and hold and sometimes torture them) and home-made rocket attacks that I think had killed eight.</p>
    <ul>
-      <li>16Jul06 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20060716_IsraeliEmbassy.pdf" target="_blank">Letter to Israeli Ambassador</a> (21kb).</li>
-      <li>16Jul06 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20060717_WarnerAllenMoran.pdf" target="_blank">Letter to Senators Warner &amp; Allen and Congressman Moran</a> and the <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20060717_Warner_reply.pdf">reply from Warner</a> (794kb) and the <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20060717_Moran_reply.pdf">reply from Moran</a> (815kb)</li>
+      <li>16-Jul-06 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20060716_IsraeliEmbassy.pdf" target="_blank">Letter to Israeli Ambassador</a> (21kb).</li>
+      <li>16-Jul-06 <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20060717_WarnerAllenMoran.pdf" target="_blank">Letter to Senators Warner &amp; Allen and Congressman Moran</a> and the <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20060717_Warner_reply.pdf">reply from Warner</a> (794kb) and the <a href="<?= $rootRelativeUrl ?>timeforjustice/files/20060717_Moran_reply.pdf">reply from Moran</a> (815kb)</li>
    </ul>
 </div>
 
@@ -82,7 +88,7 @@
    <p>I agree that their current president Ahmadinejad appears to be a bit of a nutcase but I suspect he is playing to a domestic audience to draw attention away from economic problems and in any case, the real power in Iran lies with the unelected Mullahs.</p>
    <p>However, my real wish is that we can learn to see the world from the Iranian position. Iran actually has good reason to feel threatened and if we understand that, perhaps we can reduce the pressure. After all, the CIA backed a <a href="http://en.wikipedia.org/wiki/1953_Iranian_coup_d%27%C3%A9tat" target="_blank" class="external">coup in 1953</a> that toppled the elected government and then backed the Shah, despite his reppressive rule. A popular uprising led by Islamic clerics overthrew the Shah and they have been in power ever since.  During the <a href="http://en.wikipedia.org/wiki/Iran-Iraq_War" target="_blank" class="external">Iran-Iraq war</a> which killed or wounded a half-million Iranians, the US actively aided Saddam Hussein's Iraq even though Iraq was the aggressor. Now Iran faces instability on both its western border with Iraq and its eastern border with Afghanistan. The &quot;Axis of Evil&quot; speech certainly did nothing to reassure them of our benign intentions.</p>
    <ul>
-      <li>3Aug07 <a href="<?= $rootRelativeUrl ?>files/20070803_NPR.pdf" target="_blank">Email to NPR</a> (28kb).</li>
+      <li>3-Aug-07 <a href="<?= $rootRelativeUrl ?>files/20070803_NPR.pdf" target="_blank">Email to NPR</a> (28kb).</li>
    </ul>
 </div>
 
